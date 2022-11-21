65af4e58c5fd99e779c5a937a86641c16287dc30
Derick Germano <derick_gdo@hotmail.com>
2022-11-21 19:09:12 -0300

    teste

--- HTML-CSS/HTML/cadastroDoador.js
+++ HTML-CSS/HTML/cadastroDoador.js
@@ -7,14 +7,6 @@ let cpf = document.querySelector('#cpf')
 let senha = document.querySelector('#senha')
 let confirmsenha = document.querySelector('#confirmsenha')
 
-nome.addEventListener('keyup', () => {
-        if (nome.value.length <= 2) {
-            titulo.setAttribute('style', 'color: red')
-        } else {
-            titulo.setAttribute('style', 'color: green')
-        }
-    })
-
 function cadastrar(){
     alert('botão clicado')
 }
